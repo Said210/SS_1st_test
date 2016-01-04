38198058b7efa1ec042813a6b17ba412929af015
Said Pelaez <said@cloro.mx>
2016-01-04 00:52:12 -0600

    i

--- ss/ViewController.swift
+++ ss/ViewController.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2016 eulr. All rights reserved.
 //
 
+import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var alert: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func login(sender: AnyObject) {
+        var comp =  "Tus datos fueron: email: "
+        comp += email.text!
+        comp += "\n password: "
+        comp += password.text!
+        alert.text = comp
+    }
 
 }
 
